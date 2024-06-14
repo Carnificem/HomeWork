f8d2ffddd2fb17fab4fdb1fca3e48548f8b75f12
Carnificem <Carnificem1993@gmail.com>
2024-06-14 12:58:18 +0300

    Измененный коммит

--- src/main/java/exception/EmployeeStorageIsFullException.java
+++ src/main/java/exception/EmployeeStorageIsFullException.java
@@ -3,9 +3,25 @@ package exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class EmployeeStorageIsFullException extends RuntimeException {
+    public EmployeeStorageIsFullException(){
 
-public class EmployeeStorageIsFullException extends Exception {
+    }
     public EmployeeStorageIsFullException(String massage) {
         super(massage);
     }
+
+    public EmployeeStorageIsFullException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EmployeeStorageIsFullException(Throwable cause) {
+        super(cause);
+    }
+
+    public EmployeeStorageIsFullException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
 }
